f5e96209a8e6f2e0b306c77ea4840b97214ca8b3
Chris Robertson <chrisxrobertson@gmail.com>
2017-10-13 09:14:10 +1100

    updated install script

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 # Create symlinks
-ln -sv ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -sv ~/.dotfiles/.alias ~/.alias
+ln -sv ~/.dotfiles/.bashrc ~/.bashrc
+ln -sv ~/.dotfiles/.gitignore_global ~/.gitignore_global
+ln -sv ~/.dotfiles/.ideavimrc ~/.ideavimrc
+ln -sv ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sv ~/.dotfiles/.vimrc ~/.vimrc
+ln -sv ~/.dotfiles/.zshrc ~/.zshrc
