e3446eb2c1042e1e8cfc20dcb2e4ca37f23e33ec
Robertson, Chris <chris.robertson@auspost.com.au>
2021-09-07 23:41:47 +1000

    Add spacemacs to emacs setup

--- setup_emacs.sh
+++ setup_emacs.sh
@@ -2,7 +2,8 @@
 
 # backup emacs config if it exists
 [ -f ~/.emacs ] && mv ~/.emacs ~/.emacs.bak
-[ -d ~/.emacs.d ] && mv ~/.emacs.d ~/.emacs.default
+[ -d ~/.emacs.d ] && mv ~/.emacs.d ~/.emacs.d.bak
+[ -f ~/.emacs-profiles.el ] && mv ~/.emacs-profiles.el ~/.emacs-profiles.el.bak
 
 # install chemacs
 git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
@@ -10,4 +11,7 @@ git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
 # link chemacs config
 ln -sv ~/.dotfiles/.emacs-profiles.el ~/.emacs-profiles.el
 
+# link spacemacs config
+mkdir ~/.spacemacs.d
+ln -sv ~/.dotfiles/spacemacs/init.el ~/.spacemacs.d/init.el
 
