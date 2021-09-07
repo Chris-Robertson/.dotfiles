0a8b9d79a6e3105555d4c44f7cf294b5ad0b2250
Chris Robertson <chris.robertson@auspost.com.au>
2021-09-08 08:54:16 +1000

    Update emacs setup script

--- setup_emacs.sh
+++ setup_emacs.sh
@@ -2,8 +2,9 @@
 
 # backup emacs config if it exists
 [ -f ~/.emacs ] && mv ~/.emacs ~/.emacs.bak
-[ -d ~/.emacs.d ] && mv ~/.emacs.d ~/.emacs.d.bak
 [ -f ~/.emacs-profiles.el ] && mv ~/.emacs-profiles.el ~/.emacs-profiles.el.bak
+[ -d ~/.emacs.d ] && mv ~/.emacs.d ~/.emacs.d.bak
+[ -d ~/.spacemacs.d ] && mv ~/.spacemacs.d ~/.spacemacs.d.bak
 
 # install chemacs
 git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
